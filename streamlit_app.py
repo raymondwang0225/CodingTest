e4df91beaa8c6ea4e15f705bf9822a6ca2a34454
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-23 05:59:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,11 @@ def get_data_B(url):
 
 # 呼叫函數，取得data_B
 url = "https://ordapi.bestinslot.xyz/v1/get_collection_snapshot/bitcoin-frogs-snapshot.json"
-data = get_data_B(url)
-
+#data = get_data_B(url)
+data = {
+    "first column": [1, 2, 3, 4],
+    "second column": [10, 20, 30, 40],
+}
 # 印出data_B檢查結果
 #print(data_B)
 
@@ -36,19 +39,4 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 st.title("Bitcoin Frogs Holding Data")
 
 #row size 35 px
-st.dataframe(df,height=630,use_container_width =True,column_config={
-        "Rank": st.column_config.Column(
-            "Rank",
-            width = "small",
-            help="Show rank order",
-        ),
-        "Holding %": st.column_config.ProgressColumn(
-            "Holding %",
-            width = "large",
-            help="Show Holding Percentage",
-            format=" %.4f%%",
-            min_value=0,
-            max_value=10,
-        ),
-       
-    },hide_index=True,)
+st.dataframe(df,height=630,use_container_width =True,hide_index=True)
