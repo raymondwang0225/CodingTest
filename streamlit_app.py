ff8fb4f9d73d6112b8a8728f5a76c61db4ac6933
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-23 05:49:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import requests
 import pandas as pd
 
 
-data_B  =  {
-            "first column": [1, 2, 3, 4],
-            "second column": [10, 20, 30, 40],
-        }
+data_B = {
+"first column": [1, 2, 3, 4],
+"second column": [10, 20, 30, 40],
+}
 # 將data_B轉換成DataFrame
 df = pd.DataFrame(data_B)
 
 
 
-st.title("Bitcoin Frogs Holding Data")
+#st.title("Bitcoin Frogs Holding Data")
 
 st.table(df)
