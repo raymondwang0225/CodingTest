cd58f88db56ba6aa7b1f1735a0d76f39fff5c473
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-23 05:19:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,11 @@ def get_data_B(url):
 def main():
     # 呼叫函數，取得data_B
     url = "https://ordapi.bestinslot.xyz/v1/get_collection_snapshot/bitcoin-frogs-snapshot.json"
-    data_B = get_data_B(url)
+    #data_B = get_data_B(url)
     
     # 印出data_B檢查結果
     #print(data_B)
-    
+    data_B =[{'rank': 4367, 'wallet': 'bc1qzs85dl3q7pmvnju20wngzu3czcurul4qhehn2q', 'inscriptions_count': 1, 'Holding %': 0.01}]
     # 將data_B轉換成DataFrame
     df = pd.DataFrame(data_B)
     
@@ -41,7 +41,22 @@ def main():
     st.title("Bitcoin Frogs Holding Data")
     
     #row size 35 px
-    st.dataframe(df,height=630,use_container_width =True,hide_index=True,)
+    st.dataframe(df,height=630,use_container_width =True,column_config={
+            "Rank": st.column_config.Column(
+                "Rank",
+                width = "small",
+                help="Show rank order",
+            ),
+            "Holding %": st.column_config.ProgressColumn(
+                "Holding %",
+                width = "large",
+                help="Show Holding Percentage",
+                format=" %.4f%%",
+                min_value=0,
+                max_value=10,
+            ),
+           
+        },hide_index=True,)
 
 
 
