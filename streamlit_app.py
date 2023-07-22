319f1093c7c9f196e63ee0f7d5433023ddbcf417
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-23 05:39:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,9 @@
 import requests
 import pandas as pd
 
-data_B=[]
 
-
-def get_data_B(url):
-    # 使用requests模組取得json數據(data_A)
-    response = requests.get(url)
-    data_A = response.json()
-
-    # 調整data_A成新的內容(data_B)
-    data_B = [{"Rank": n,"Wallet": item["wallet"],"Inscriptions Count": item["Inscriptions_count"],"Holding %": round(item["inscriptions_count"] / 100, 4)} for n, item in enumerate(data_A, start=1)]
-
-    return data_B
-
-
-# 呼叫函數，取得data_B
-url = "https://ordapi.bestinslot.xyz/v1/get_collection_snapshot/bitcoin-frogs-snapshot.json"
-#data_B = get_data_B(url)
-
-# 印出data_B檢查結果
-#print(data_B)
-data_B =[{'rank': 4367, 'wallet': 'bc1qzs85dl3q7pmvnju20wngzu3czcurul4qhehn2q', 'inscriptions_count': 1, 'Holding %': 0.01},{'rank': 4367, 'wallet': 'bc1qzs85dl3q7pmvnju20wngzu3czcurul4qhehn2q', 'inscriptions_count': 1, 'Holding %': 0.01}]
+data_B  = ['Geeks', 'For', 'Geeks', 'is', 
+            'portal', 'for', 'Geeks']
 # 將data_B轉換成DataFrame
 df = pd.DataFrame(data_B)
 
