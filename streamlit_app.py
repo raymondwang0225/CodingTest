874c6eac1898f9abfff28526cdb3fcdd82a7c276
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-23 19:02:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,13 @@ def get_average_cost(item):
     for inscription_number in item["inscriptions"]:
         url = f"https://ordapi.bestinslot.xyz/v1/get_inscription_with_number/{inscription_number}"
         response = requests.get(url)
-        inscription_data = response.json()
+        
+        try:
+            response.raise_for_status()  # Check if the request was successful
+            inscription_data = response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Error while fetching inscription data for {inscription_number}: {e}")
+            continue
 
         transfers = inscription_data.get("transfers", [])
         last_transfer = transfers[-1] if transfers else None
@@ -25,14 +31,18 @@ def get_average_cost(item):
                 total_cost += last_transfer["psbt_sale"]
 
     average_cost = total_cost / len(item["inscriptions"]) if len(item["inscriptions"]) > 0 else 0
-    
     return average_cost
-
 
 def get_holding_output(url):
     # 使用requests模組取得json數據(holding_input)
     response = requests.get(url)
-    holding_input = response.json()
+
+    try:
+        response.raise_for_status()  # Check if the request was successful
+        holding_input = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error while fetching holding data: {e}")
+        return []
 
     # 調整holding_input成新的內容(holding_output)並新增"Holding %"和"average cost"屬性
     holding_output = []
@@ -42,12 +52,10 @@ def get_holding_output(url):
             "Rank": n,
             "Wallet": item["wallet"],
             "Inscriptions Count": item["inscriptions_count"],
-            "Holding %": round(item["inscriptions_count"] / 100, 4),
+            "Holding %": round(item["inscriptions_count"] / 10000, 4),
             "Average Cost": average_cost
         })
-
     return holding_output
-
 
 # Streamlit App
 def main():
